259baaf3816394fd87d029ee42d51a89cc0c61cf
123951494 <123951494@qq.com>
2018-04-08 15:30:42 +0800

    拼音方法

--- Flew.Test/UnitTest1.cs
+++ Flew.Test/UnitTest1.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Flew.Util.ComHelper;
+using System.Diagnostics;
 
 namespace Flew.Test
 {
@@ -9,6 +11,10 @@ namespace Flew.Test
         [TestMethod]
         public void TestMethod1()
         {
+            var IpAddress = IPAddress.GetIp();
+            Trace.WriteLine(string.Format("输出：{0}", IpAddress));
         }
     }
 }
+
+
---
+++ Flew.Util/StrHelper/PinYin.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NPinyin;
+namespace Flew.Util.StrHelper
+{
+    /// <summary>
+    /// 汉字转拼音
+    /// </summary>
+    public class PinYin
+    {
+        #region 汉字转拼音     
+        /// <summary>
+        /// 单汉字转拼音
+        /// </summary>
+        /// <param name="hz"></param>
+        /// <returns></returns>
+        public static string GetPY(string hz)
+        {
+            string py = Pinyin.GetPinyin(hz);
+            return py;
+        }
+        /// <summary>
+        /// 集合转拼音
+        /// </summary>
+        /// <param name="hz"></param>
+        /// <returns></returns>
+        public static List<string> GetPY(List<string> hz)
+        {
+            List<string> pys = new List<string>(); 
+            foreach (var item in hz)
+            {
+                string py = Pinyin.GetPinyin(item);
+                pys.Add(py);
+            }
+            return pys;
+        }
+        #endregion
+        #region 汉字转拼音简码
+        /// <summary>
+        /// 汉字转简码
+        /// </summary>
+        /// <param name="hz"></param>
+        /// <returns></returns>
+        public static string GetPYJM(string hz,Encoding code)
+        {
+            //Encoding gb2312 = Encoding.GetEncoding("GB2312");
+            string jm = Pinyin.GetInitials(hz,code);
+            return jm;
+        }
+
+        /// <summary>
+        /// 汉字集合转拼音简码
+        /// </summary>
+        /// <param name="hz"></param>
+        /// <returns></returns>
+        public static List<string> GetPYJM(List<string> hz, Encoding code)
+        {
+            List<string> pys = new List<string>();
+            foreach (var item in hz)
+            {
+                string py = Pinyin.GetInitials(item,code);
+                pys.Add(py);
+            }
+            return pys;
+        }
+        #endregion
+    }
+}
